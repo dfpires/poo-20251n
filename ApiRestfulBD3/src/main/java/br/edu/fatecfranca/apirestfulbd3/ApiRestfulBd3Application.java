d1a733d63d6fc05e453b920e6abb941c5ca24527
Daniel <dfpires@gmail.com>
2025-05-23 21:59:34 -0300

    API Restful BD

---
+++ ApiRestfulBD3/src/main/java/br/edu/fatecfranca/apirestfulbd3/ApiRestfulBd3Application.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.apirestfulbd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulBd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulBd3Application.class, args);
+	}
+
+}
---
+++ ApiRestfulBD3/src/test/java/br/edu/fatecfranca/apirestfulbd3/ApiRestfulBd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.apirestfulbd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulBd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
